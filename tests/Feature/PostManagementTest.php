bbdb959211db97d28c61d4f4f6a82867f152cde5
giovannyg <giovanny.gop@gmail.com>
2020-06-09 22:11:52 -0500

    PostFactory Added to implement posts list test function

--- tests/Feature/PostManagementTest.php
+++ tests/Feature/PostManagementTest.php
@@ -10,7 +10,18 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class PostManagementTest extends TestCase
 {
   use RefreshDatabase;
-  
+
+    /** @test */
+    public function a_list_can_be_retrieved()
+    {
+      $this->withoutExceptionHandling();
+
+      factory(Post::class, 3)->create(); //Testing data
+
+      $response = $this->get('/posts'); //Requesting the route
+      $response->assertOk();
+    }
+
   /** @test */
   public function a_post_can_be_created()
   {
