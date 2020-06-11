18186dfbccb4f477ec0ec0a5a0ecf2211238cb5a
giovannyg <giovanny.gop@gmail.com>
2020-06-11 11:44:09 -0500

    Missing CRUD functions added, CRUD feature testing is done

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,12 +7,39 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-  public function store(Request $request)
+  public function index() 
   {
-    $data = $request->validate([
+    $posts = Post::all();
+    return view('posts.index', compact('posts'));
+  }
+
+  public function show(Post $post)
+  {
+    return view('posts.show', compact('post'));
+  }
+
+  public function store()
+  {
+    $data = request()->validate([
+      'title' => 'required',
+      'content' => 'required'
+    ]);
+    $post = Post::create($data);
+    return redirect('/posts/' . $post->id);
+  }
+
+  public function update(Post $post)
+  {
+    $data = request()->validate([
       'title' => '',
       'content' => ''
     ]);
-    Post::create($data);
+    $post->update($data);
+    return redirect('/posts/' . $post->id);
+  }
+  public function destroy(Post $post)
+  {
+    $post->delete();
+    return redirect('/posts/');
   }
 }
--- tests/Feature/PostManagementTest.php
+++ tests/Feature/PostManagementTest.php
@@ -11,16 +11,39 @@ class PostManagementTest extends TestCase
 {
   use RefreshDatabase;
 
-    /** @test */
-    public function a_list_can_be_retrieved()
-    {
-      $this->withoutExceptionHandling();
+  /** @test */
+  public function a_list_of_posts_can_be_retrieved()
+  {
+    $this->withoutExceptionHandling();
 
-      factory(Post::class, 3)->create(); //Testing data
+    factory(Post::class, 3)->create(); //Testing data
 
-      $response = $this->get('/posts'); //Requesting the route
-      $response->assertOk();
-    }
+    $response = $this->get('/posts'); //Requesting the route
+    $response->assertOk();
+
+    $posts = Post::all();
+
+    $response->assertViewIs('posts.index');
+    $response->assertViewHas('posts', $posts);
+
+  }
+
+  /** @test */
+  public function a_post_can_be_retrieved()
+  {
+    $this->withoutExceptionHandling();
+
+    $post = factory(Post::class)->create(); //Testing data
+
+    $response = $this->get('/posts/' . $post->id); //Requesting the route
+    
+    $response->assertOk();
+
+    $post = Post::first();
+
+    $response->assertViewIs('posts.show');
+    $response->assertViewHas('post', $post);
+  }
 
   /** @test */
   public function a_post_can_be_created()
@@ -30,11 +53,66 @@ class PostManagementTest extends TestCase
       'title' => 'Test',
       'content' => 'Test content'
     ]);
-
-    $response->assertOk();
+    
     $this->assertCount(1, Post::all());
     $post = Post::first();
+
     $this->assertEquals($post->title, 'Test');
     $this->assertEquals($post->content, 'Test content');
+
+    $response->assertRedirect('/posts/' . $post->id);
+  }
+
+  /** @test */
+  public function post_title_is_required()
+  {
+    $response = $this->post('/posts', [
+      'title' => '',
+      'content' => ''
+    ]);
+    $response->assertSessionHasErrors(['title']);
+  }
+
+  /** @test */
+  public function post_content_is_required()
+  {
+    $response = $this->post('/posts', [
+      'title' => 'Title',
+      'content' => ''
+    ]);
+    $response->assertSessionHasErrors(['content']);
+  }
+
+  /** @test */
+  public function a_post_can_be_updated()
+  {
+    $this->withoutExceptionHandling();
+    $post = factory(Post::class)->create(); //Testing data
+
+    $response = $this->put('/posts/' . $post->id, [
+      'title' => 'Test',
+      'content' => 'Test content'
+    ]);
+    
+    $this->assertCount(1, Post::all());
+    $post = $post->fresh();
+
+    $this->assertEquals($post->title, 'Test');
+    $this->assertEquals($post->content, 'Test content');
+
+    $response->assertRedirect('/posts/' . $post->id);
+  }
+
+  /** @test */
+  public function a_post_can_be_deleted()
+  {
+    $this->withoutExceptionHandling();
+    $post = factory(Post::class)->create(); //Testing data
+
+    $response = $this->delete('/posts/' . $post->id);
+    
+    $this->assertCount(0, Post::all());
+
+    $response->assertRedirect('/posts/');
   }
 }
